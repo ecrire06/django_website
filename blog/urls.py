40c574682b42737fb0b4788df8d1fc7e85045898
ecrire06 <juneha2002@gmail.com>
2021-08-21 11:40:18 +0900

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,12 +5,25 @@ https://docs.python.org/3/howto/regex.html
 """
 
 from django.conf.urls import url
-from django.urls import include, path, re_path
+from django.urls import include, path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    # post views
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+    # This is a previous version of writing an urlpatterns (using regular expression)
+#    url(r'^$', views.post_list, name='post_list'),
+#    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+#        r'(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
+
+    # This is a new version of writing an urlpatterns [django ver 2.0 and after]
+#     post views
+#        Check views.py in blog directory
+#        I. if you want to use "def post_list(request)"
+#            set path('', views.post_list ~)
+#        II. if you want to use "class PostListView(ListView)"
+#            set path('', views.PostListView.as_view() ~)
+#    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>', views.post_detail, name='post_detail'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -71,6 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
+# e-mail SMTP server
+EMAIL_FROM = 'ecrire06@korea.ac.kr'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ecrire06@korea.ac.kr'
+EMAIL_HOST_PASSWORD = 'Vo1@tus28'
+EMAIL_USE_TLS = True
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -106,13 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
